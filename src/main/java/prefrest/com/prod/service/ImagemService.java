a0c55f58632a2e47045d9127f6961f84c9f6dce9
faqtt0 <faqtt0@gmail.com>
2018-04-03 21:25:39 -0300

    Parcial Imagem

--- src/main/java/prefrest/com/prod/service/ImagemService.java
+++ src/main/java/prefrest/com/prod/service/ImagemService.java
@@ -72,7 +72,7 @@ public class ImagemService {
                     ImageWriteParam param = writer.getDefaultWriteParam();
                     if (param.canWriteCompressed()){
                         param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-                        param.setCompressionQuality(0.05f);
+                        param.setCompressionQuality(0.5f);
                     }
 
                     writer.write(null, new IIOImage(image, null, null), param);
