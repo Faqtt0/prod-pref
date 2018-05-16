bd64a345c0bb077268520d318da89b6852d78c87
fagner <fagner@viasoft.com.br>
2018-05-16 10:17:14 -0300

    Ajuste para gerar war

--- src/main/java/prefrest/com/prod/ProdApplication.java
+++ src/main/java/prefrest/com/prod/ProdApplication.java
@@ -2,12 +2,19 @@ package prefrest.com.prod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import prefrest.com.prod.config.property.ControleProperty;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ControleProperty.class)
-public class ProdApplication {
+public class ProdApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProdApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdApplication.class, args);
