75cd00a7bc4ba4d84150bc30a0e573fb3697f638
fagner <faqtt0@gmail.com>
2018-06-26 09:07:26 -0300

    Ajuste deletar permissões do usuário.

--- src/main/java/prefrest/com/prod/repository/usuario/UsuarioPermissaoRepository.java
+++ src/main/java/prefrest/com/prod/repository/usuario/UsuarioPermissaoRepository.java
@@ -1,6 +1,8 @@
 package prefrest.com.prod.repository.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import prefrest.com.prod.model.UsuarioPermissao;
 import prefrest.com.prod.model.UsuarioPermissaoID;
 
@@ -8,6 +10,13 @@ import java.util.List;
 
 public interface UsuarioPermissaoRepository extends JpaRepository<UsuarioPermissao, UsuarioPermissaoID> {
     List<UsuarioPermissao> findAllByCodTipoUsuarioOrderByCodPermissao(Long codigoTipoUser);
+
+    @Transactional
+    @Modifying
+    /*@Query(nativeQuery = true, value = "DELETE FROM USUARIOPERMISSAO WHERE CODTIPOUSUARIO = ?1")*/
     int deleteAllByCodTipoUsuario(Long codTipoUsuario);
+
+    @Transactional
+    @Modifying
     int deleteByCodPermissaoAndCodTipoUsuario(Long codPermissao, Long codTipoUser);
 }
